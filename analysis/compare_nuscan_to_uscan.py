a1ce9288a048113912cdccb2d0dfb00805bc1d48
Joseph Howie <josephhowie@Josephs-MacBook-Air.local>
2023-06-07 15:07:41 -0700

    added JSD

--- analysis/compare_nuscan_to_uscan.py
+++ analysis/compare_nuscan_to_uscan.py
@@ -5,7 +5,7 @@ import pandas as pd
 import sys
 import warnings
 warnings.filterwarnings("ignore")
-## compares differences between DP method and L-CLT computed P[e, epsilon] values and times to compute edges 
+## compares differences between DP method and L-CLT computed P[e, epsilon] values and times to compute edges
 ## ./compare_nuscan_to_uscan.py <path-to-.prob_uscan> <path-to-.prob_nuscan>
 
 def main():
@@ -22,19 +22,21 @@ def main():
     nuscan = pd.read_csv(n_file, sep="\s+",names=columns, on_bad_lines='skip')#, dtype=dtye)
 
     #df['column_name'] = np.isclose(df['column_name'].values, df['column_name'].astype(int).values)
-    
+
     # merge uscan and nuscan
     combine = pd.merge(uscan,nuscan, on=["u", 'v', 'k'])
     # keep only edges with large enough ku and positive P[e, epsilon]
     clamp = 1e-300
     compare = combine.loc[(combine.k>=thres) & (combine.p_x>clamp) & (combine.p_y>clamp) ]
     #print(compare.sort_values(by=["k", "t_x", "t_y"]))
-    # call methods for computing K-L divergence, RMSE, time saved, 
+    # call methods for computing K-L divergence, RMSE, time saved,
     #print(r"Number of edges with non-zero P[e, epsilon] that used Lyapunov CLT = {}".format(len(compare)))
     p_x = compare.p_x.to_numpy()
     p_y = compare.p_y.to_numpy()
 
-    kl_div = kl_divergence(p_x, p_y)
+    #kl_div = kl_divergence(p_x, p_y)
+    Mval = M_val(p_x, p_y)
+    jsd = 0.5 * kl_divergence(p_x, Mval) + 0.5 * kl_divergence(p_y, Mval)
     #print(f"K-L divergence = {kl_div:.4f}")
 
     rmse = RMSE(p_x, p_y)
@@ -42,19 +44,25 @@ def main():
 
     t_x = compare.t_x.to_numpy()
     t_y = compare.t_y.to_numpy()
-    
+
     tr, ts = time_saved(t_x, t_y)
     #print(f"The time saved by NUSCAN is {ts:.4f} seconds  which is a {tr:.4f}x factor speed up")
-    print(eta, eps, mu, thres-2, len(compare), kl_div, rmse, tr, ts, t_x.sum(), t_y.sum())
-    
+    print(eta, eps, mu, thres-2, len(compare), jsd, rmse, tr, ts, t_x.sum(), t_y.sum())
+
 def norm_probs(p):
     '''p is a numpu array'''
     return p/p.sum()
-    
-def kl_divergence(pu, pn):
-    pu_norm = norm_probs(pu)
-    pn_norm = norm_probs(pn)
-    res_arr = [pp*np.log(pp/qq) for pp, qq in zip(pu_norm, pn_norm)]
+
+def M_val(p, q):
+    p_norm = norm_probs(p)
+    q_norm = norm_probs(q)
+    res_arr = np.array([(pp+qq)/2 for pp, qq in zip(p_norm, q_norm)])
+    return res_arr
+
+def kl_divergence(p, q):
+    p_norm = norm_probs(p)
+    q_norm = norm_probs(q)
+    res_arr = [pp*np.log(pp/qq) for pp, qq in zip(p_norm, q_norm)]
     K_L_div = sum(res_arr)
     return K_L_div
 
@@ -71,7 +79,7 @@ def time_saved(tu, tn):
         else:
             return (np.inf, tut - tnt)
     return (tut/tnt, tut - tnt)
-    
+
 
 if __name__=="__main__":
     main()
